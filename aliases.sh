783ccb6b7e79241988b2b3a0bfa866fc13ac545d
Pascal Le Merrer <pascal.lemerrer@gmail.com>
2017-08-12 15:13:41 +0200

    Initial commit

---
+++ aliases.sh
@@ -0,0 +1,29 @@
+#!/usr/local/bin/fish
+
+alias rm "rm -i"
+funcsave rm
+
+alias g "git"
+funcsave g
+
+alias gs "git status -b"
+funcsave gs
+
+alias ga "git add "
+funcsave ga
+
+alias gb "git branch "
+funcsave gb
+
+alias gc "git commit"
+funcsave gc
+
+alias gd "git diff"
+funcsave gd
+
+alias go "git checkout"
+funcsave go
+
+alias gk "gitk --all"
+funcsave gk
+
